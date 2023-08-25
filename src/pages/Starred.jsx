315adaf8ba728a0ed9e19064372417fc2c471d41
Vinay Kumar <vinaykumary122@gmail.com>
2023-08-26 00:46:15 +0530

    installed react-router-dom

---
+++ src/pages/Starred.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Starred = () => {
+    return(
+        <div>
+            <h1>Starred page</h1>
+            <Link to="/">Go to Home</Link>
+        </div>
+    )
+}
+
+export default Starred;
