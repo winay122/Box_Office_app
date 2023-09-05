d54ce7798c8e1169f8eeafd37afddb68eb369400
Vinay Kumar <vinaykumary122@gmail.com>
2023-09-05 23:49:57 +0530

    added searchbox for search show

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = ()=>{
+
+    const [searchStr, setSearchStr] = useState('');
+
+    const onSearchInputChange = (event) => {
+        
+        setSearchStr(event.target.value);
+    };
+
+    const onSearch =async (event) => {
+        event.preventDefault();
+
+        const response = await fetch(
+            `https://api.tvmaze.com/search/shows?q=${searchStr}`);
+        const body = await response.json();
+    
+        console.log(body)
+
+        //https://api.tvmaze.com/search/shows?q=girls
+    }
+
     return(
         <div>
-            <h1>Home</h1>
-            <Link to="/starred">Go to Starred page</Link>
+            <form onSubmit={onSearch}>
+                <input type="text" value={searchStr} onChange={onSearchInputChange} />
+                <button type="submit">Update</button>
+            </form>
         </div>
     );
 }
